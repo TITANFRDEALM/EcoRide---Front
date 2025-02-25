2054ec2e854e0512a8324dfaebd5c9153d3bcbd4
TITANFR <titanfrmax@gmail.com>
2025-02-25 10:47:10 +0100

    add auth (signin, signup, account)

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,7 +5,10 @@ import Route from "./Route.js";
 export const allRoutes = [
 
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/reservation", "Reservation", "/pages/reservation.html")];
+    new Route("/reservation", "Reservation", "/pages/reservation.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon Compte", "/pages/auth/account.html")];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 
