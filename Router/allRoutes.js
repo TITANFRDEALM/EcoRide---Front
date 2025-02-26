373c172b9bbc6e030290b4b8a639a0dcaeb629bd
TITANFR <titanfrmax@gmail.com>
2025-02-26 10:50:45 +0100

    auth et role

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,11 +4,11 @@ import Route from "./Route.js";
 
 export const allRoutes = [
 
-    new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/reservation", "Reservation", "/pages/reservation.html"),
-    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html", "/js/auth/signup.js"),
-    new Route("/account", "Mon Compte", "/pages/auth/account.html")];
+    new Route("/", "Accueil", "/pages/home.html", []),
+    new Route("/reservation", "Reservation", "/pages/reservation.html", []),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"], "/js/auth/signin.js"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html", ["disconnected"], "/js/auth/signup.js"),
+    new Route("/account", "Mon Compte", "/pages/auth/account.html", ["client", "admin"])];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 
